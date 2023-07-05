a7cb358f0594a42e6fbc4a8a4bab7c6844b11e76
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-07-05 12:24:04 +0200

    Finish style to aftermovies

--- src/pages/Home/AfterMovies/index.jsx
+++ src/pages/Home/AfterMovies/index.jsx
@@ -3,8 +3,15 @@ import './style.css';
 import { Link } from 'react-router-dom';
 
 export const AfterMovies = () => {
-  return <section className="aftermovies" id="aftermovies">
-    <h1>Aftermovies</h1>
-    <div>play</div>
-  </section>;
+  return (
+    <section className="aftermovies" id="aftermovies">
+      <h1 className="aftermovies__title">Aftermovies</h1>
+      <a
+        href="https://www.youtube.com/watch?v=EErY75MXYXI"
+        className="aftermovies__link"
+      >
+        <div className="aftermovies__box"></div>
+      </a>
+    </section>
+  );
 };
