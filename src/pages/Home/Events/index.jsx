64636faef0902c4f7a67f69dbb387f45facd15ed
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-07-06 20:41:17 +0200

    Finish style and responsivity of events

--- src/pages/Home/Events/index.jsx
+++ src/pages/Home/Events/index.jsx
@@ -6,6 +6,18 @@ import { Link } from 'react-router-dom';
 export const Events = () => {
   const [events, setEvents] = useState(null);
 
+  const images = [
+    require('./img/img_1.png'),
+    require('./img/img_2.png'),
+    require('./img/img_3.png'),
+    require('./img/img_1.png'),
+    require('./img/img_2.png'),
+    require('./img/img_3.png'),
+    require('./img/img_1.png'),
+    require('./img/img_2.png'),
+    require('./img/img_3.png'),
+  ];
+
   useEffect(() => {
     LoadEvents().then((events) => setEvents(events));
   }, []);
@@ -16,11 +28,27 @@ export const Events = () => {
         <>
           <h2 className="events__title">Kalendář akcí</h2>
           <div className="events__box">
-            {events.map((event) => (
-              <Link to={`event/${event.id}`} key={event.id}>
-                <button className="event">
-                  <h3 className="event__city">{event.city_event}</h3>
-                  <p className="event__date">{event.date_of_event}</p>
+            {events.map((event, index) => (
+              <Link
+                to={`event/${event.id}`}
+                key={event.id}
+                className="event__link"
+              >
+                <button
+                  className="event"
+                  style={{
+                    backgroundImage: `url(${images[index]})`,
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                  }}
+                >
+                  <div className="event-hover">
+                    <h3 className="event__city">{event.city_event}</h3>
+                    <p className="event__date">
+                      {event.date_of_event}
+                    </p>
+                  </div>
                 </button>
               </Link>
             ))}
