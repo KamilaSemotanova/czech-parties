02a3268d288c365e70a453a1496ac07f83f1067e
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-07-14 14:25:01 +0200

    Edit error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
 import './style.css';
+import { Link } from 'react-router-dom';
 
 export const ErrorPage = () => {
   return (
-    <section>
-      <h1>Error 404</h1>
+    <section className="error">
+      <div className="error__box">
+        <h1 className="error__title">404</h1>
+        <p>-</p>
+        <p className="error__undertitle">stránka nebyla nalezena</p>
+        <p className="error__text">
+          Stránka, kterou hledáte neexistuje, byla odstraněna nebo se změnila.
+        </p>
+        <Link to="/">
+          <button className="error__homebtn">Domů</button>
+        </Link>
+      </div>
     </section>
   );
 };
