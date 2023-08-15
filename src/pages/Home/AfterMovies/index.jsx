200a90eb25423c926cdf01ff764ac4cfd660a89d
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-08-15 12:32:41 +0200

    Add new link to aftermovies

--- src/pages/Home/AfterMovies/index.jsx
+++ src/pages/Home/AfterMovies/index.jsx
@@ -7,7 +7,7 @@ export const AfterMovies = () => {
     <section className="aftermovies" id="aftermovies">
       <h1 className="aftermovies__title">Aftermovies</h1>
       <a
-        href="https://www.youtube.com/watch?v=EErY75MXYXI"
+        href="https://www.youtube.com/watch?v=rrcLss3FhJk&ab_channel=CzechParties"
         className="aftermovies__link"
       >
         <div className="aftermovies__box"></div>
