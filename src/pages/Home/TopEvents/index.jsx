6f14e9646b6a4e6d7bf2bdc346226617128d3056
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-08-17 11:50:26 +0200

    Add top events modification

--- src/pages/Home/TopEvents/index.jsx
+++ src/pages/Home/TopEvents/index.jsx
@@ -1,28 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
-import { CountdownButton } from './Countdown';
+import { LoadEventById } from '../../../functions/db';
+import { Link } from 'react-router-dom';
 
 export const TopEvents = () => {
-  const handleClick = (event) => {
-    event.preventDefault();
-    location.href = 'https://partyexpress.cz/rezervace/rezervace-2';
-  };
+  const [event1, setEvent1] = useState();
+  const [event2, setEvent2] = useState();
+  const id1 = 10001;
+  const id2 = 10002;
+
+  useEffect(() => {
+    LoadEventById(id1.toString()).then((event) => {
+      setEvent1(event);
+    });
+  }, []);
+
+  useEffect(() => {
+    LoadEventById(id2.toString()).then((event) => {
+      setEvent2(event);
+    });
+  }, []);
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const formattedDate = date.toLocaleDateString('en-GB');
+
+    return formattedDate;
+  }
+
   return (
     <section className="top" id="top__events">
-      <div className="zrce__img"></div>
-      <div className="zrce__box">
-        <h2 className="zrce__title">Zrće</h2>
-        <p className="zrce__text">Ukončení léta s CzechParties</p>
-        <button onClick={handleClick} className="zrce__btn">
-          Jedu!
-        </button>
-      </div>
-      <div className="raprezent__img"></div>
-      <div className="raprezent__box">
-        <h2 className="raprezent__title">Raprezent</h2>
-        <p className="raprezent__text">Lorem ipsum dolor sit amet</p>
-        <CountdownButton />
-      </div>
+      {event1 && (
+        <>
+          <div className="zrce__img"></div>
+          <div className="zrce__box">
+            <h2 className="zrce__title">{event1.name_of_event}</h2>
+            <p className="zrce__text">{formatDate(event1.date_of_event)}</p>
+            <Link to={`event/${event1.id}`} className="zrce__btn">
+              Koupit lístky
+            </Link>
+          </div>
+        </>
+      )}
+      {event2 && (
+        <>
+          <div className="raprezent__img"></div>
+          <div className="raprezent__box">
+            <h2 className="raprezent__title">{event2.name_of_event}</h2>
+            <p className="raprezent__text">
+              {formatDate(event2.date_of_event)}
+            </p>
+            <Link to={`event/${event2.id}`} className="raprezent__btn">
+              Koupit lístky
+            </Link>
+          </div>
+        </>
+      )}
     </section>
   );
 };
