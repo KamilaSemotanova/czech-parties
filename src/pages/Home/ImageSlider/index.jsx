31fec0eb36d0b249bbdb281018966d5d8792dfda
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-10-25 12:27:04 +0200

    Update animations onimage slider

--- src/pages/Home/ImageSlider/index.jsx
+++ src/pages/Home/ImageSlider/index.jsx
@@ -1,64 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 export const ImageSlider = () => {
-  const [currentImage, setCurrentImage] = useState(0);
-
-  const images = [
-    {
-      src: require('./img/img_1.png'),
-      text: 'Vítejte v Czechparties',
-      more: 'organizaci, která se specializuje na nezapomenutelné párty pro studenty středních a vysokých škol!',
-      even_more:
-        'Jsme tady, abychom přinesli do vašeho studentského života dávku vzrušení, radosti a neomezené zábavy.',
-      class: 'first',
-    },
-    {
-      src: require('./img/img_3.png'),
-      text: 'We are the new generation',
-      class: 'second',
-    },
-    {
-      src: require('./img/img_2.png'),
-      text: 'Katalyzátor nekonečné zábavy pro studentskou scénu!',
-      class: 'third',
-    },
-  ];
-
-  const nextImage = () => {
-    setCurrentImage((prevImage) =>
-      prevImage === images.length - 1 ? 0 : prevImage + 1,
-    );
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      nextImage();
-    }, 10000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
   return (
     <div className="slider" id="home">
-      <img
-        src={images[currentImage].src}
-        className="slider__image"
-        alt="Slider"
-      />
-      <p className={`slider__text ${images[currentImage].class}`}>
-        {images[currentImage].text}
-      </p>
-      {images[currentImage].more ? (
-        <p className={`slider__text-more`}>{images[currentImage].more}</p>
-      ) : null}
-      {images[currentImage].even_more ? (
-        <p className={`slider__text-evenmore`}>
-          {images[currentImage].even_more}
-        </p>
-      ) : null}
+      <div className="slider__images">
+        <div className="slider__slide">
+          <img
+            src={require("./img/img_1.png")}
+            className="slider__image"
+            alt="Slider"
+          />
+          <p className="slider__text first">Vítejte v Czechparties</p>
+          <p className="slider__text-more">
+            organizaci, která se specializuje na nezapomenutelné párty pro
+            studenty středních a vysokých škol!
+          </p>
+          <p className="slider__text-evenmore">
+            Jsme tady, abychom přinesli do vašeho studentského života dávku
+            vzrušení, radosti a neomezené zábavy.
+          </p>
+        </div>
+        <div className="slider__slide">
+          <img
+            src={require("./img/img_3.png")}
+            className="slider__image"
+            alt="Slider"
+          />
+          <p className="slider__text second">We are the new generation</p>
+        </div>
+        <div className="slider__slide">
+          <img
+            src={require("./img/img_2.png")}
+            className="slider__image"
+            alt="Slider"
+          />
+          <p className="slider__text third">
+            Katalyzátor nekonečné zábavy pro studentskou scénu!
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
