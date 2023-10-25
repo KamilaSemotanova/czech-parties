c73c9615c3e1f94bce1b88a4f90e470cdee27399
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-10-25 09:10:45 +0200

    Delete carousel

--- src/pages/Home/Cooperation/index.jsx
+++ src/pages/Home/Cooperation/index.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
-import './style.css';
-import { Carousel } from './Carousel';
+import React, { useState } from "react";
+import "./style.css";
+import { Carousel } from "./Carousel";
 
 export const Cooperation = () => {
   return (
     <section className="cooperation">
-      <h2 className="cooperation__title">Spolupracujeme s</h2>
-      <Carousel />
+      <h2 className="cooperation__title">Spolupráce</h2>
+      <p className="cooperation__text">
+        Pokud máte zájem o reklamu na našich akcích kontaktujte nás na
+        podpora@czechparties.cz
+      </p>
     </section>
   );
 };
